da79a221ab86b9f7f6a9960f0c1638bbd24a00fe
zaneyard <zaneyard@gmail.com>
2016-11-26 15:45:09 -0600

    modification for multiple selected step lines

--- js/algorithmSteps.js
+++ js/algorithmSteps.js
@@ -1,12 +1,15 @@
-var writeSteps = function(stepsArr, stepIndex){
+var writeSteps = function(stepsArr, selectedSteps) {
   $('#algStepsDiv').html('');
-  for(var i = 0; i<stepsArr.length; i++){
+  for (var i = 0; i < stepsArr.length; i++) {
     var html = ""
-    if(i == stepIndex){
-      html ="<span class='mainAlgStep'>"+stepsArr[i]+"</span><br />";
-    }
-    else{
-      html ="<span class='algStep'>"+stepsArr[i]+"</span><br />";
+    for (var j = 0; j < selectedSteps.length; j++) {
+      if (i == selectedSteps[j]) {
+        html = "<span class='mainAlgStep'>"+stepsArr[i]+"</span><br />";
+        break;
+      }
+      else {
+        html = "<span class='algStep'>"+stepsArr[i]+"</span><br />";
+      }
     }
     $('#algStepsDiv').append(html);
   }
