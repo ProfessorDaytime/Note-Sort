854183c5fbc8473b4d946e9eb8b912b7b33ed1d9
Karl Strickland <Stricklandk0163@my.uwstout.edu>
2016-11-28 22:33:22 -0600

    Added in songs/ drawing and merged in sorting algorithm code.

---
+++ js/visualization.js
@@ -0,0 +1,105 @@
+function draw(passedData) {
+  //Width and height
+  var w = 500;
+  var h = 100;
+  var barPadding = 1;
+  var dataset = [];
+  for(var i = 0; i<passedData.data.length-1;i++){
+    dataset.push(passedData.data[i].index);
+  }
+  //Create SVG element
+  var svg = d3.select("#myDiv")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+  svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr("x", function(d, i) {
+      return i * (w / dataset.length);
+   })
+   .attr("y", function(d) {
+      return h - (d * 4);
+   })
+   .attr("width", w / dataset.length - barPadding)
+   .attr("height", function(d) {
+      return d * 4;
+   })
+   .attr("fill", function(d) {
+    return "rgb(0, 0, " + (d * 10) + ")";
+   });
+
+  /*svg.selectAll("text")
+   .data(passedData)
+   .enter()
+   .append("text")
+   .text(function(d) {
+      return d;
+   })
+   .attr("text-anchor", "middle")
+   .attr("x", function(d, i) {
+      return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+   })
+   .attr("y", function(d) {
+      return h - (d * 4) + 14;
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", "white");*/
+}
+
+function redraw(passedData) {
+  //Width and height
+  var w = 500;
+  var h = 100;
+  var barPadding =1;
+
+  var dataset = [];
+  for(var i = 0; i<passedData.data.length-1;i++){
+    dataset.push(passedData.data[i].index);
+  }
+
+  //Create SVG element
+  var svg = d3.select("#myDiv")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+  svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr("x", function(d, i) {
+      return i * (w / dataset.length);
+   })
+   .attr("y", function(d) {
+      return h - (d * 4);
+   })
+   .attr("width", w / dataset.length - barPadding)
+   .attr("height", function(d) {
+      return d * 4;
+   })
+   .attr("fill", function(d) {
+    return "rgb(0, 0, " + (d * 10) + ")";
+   });
+
+  /*svg.selectAll("text")
+   .data(dataset)
+   .enter()
+   .append("text")
+   .text(function(d) {
+      return d;
+   })
+   .attr("text-anchor", "middle")
+   .attr("x", function(d, i) {
+      return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+   })
+   .attr("y", function(d) {
+      return h - (d * 4) + 14;
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", "white");*/
+}
